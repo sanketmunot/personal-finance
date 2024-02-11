00f371741217472281576f386f8a40d831fdacb2
sanketmunot <sanketmunot95@gmail.com>
2024-02-11 15:59:16 +0530

    auth setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Personal Finances",
@@ -13,8 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <UserButton afterSignOutUrl="/" />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
